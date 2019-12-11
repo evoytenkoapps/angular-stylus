5ac43bf20eafee4af72820a282cc5e82a0394651
Евгений Войтенко <you@example.com>
2019-12-11 23:14:28 +0300

    add RouteSupportComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,61 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import {AppComponent} from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {WebsiteNavbarComponent} from './components/website-navbar/website-navbar.component';
+import {
+  DateAdapter,
+  ErrorStateMatcher,
+  MAT_DATE_LOCALE,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatStepperIntl,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatRadioModule,
+  ShowOnDirtyErrorStateMatcher
+} from '@angular/material';
+import { RouteSupportComponent } from './components/routes/route-support/route-support.component';
 
-import { AppComponent } from './app.component';
-import { WebsiteNavbarComponent } from './components/website-navbar/website-navbar.component';
+export const desktopRoutes: Routes = [
+  {
+    path: "support",
+    component: RouteSupportComponent,
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WebsiteNavbarComponent
+    WebsiteNavbarComponent,
+    RouteSupportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    RouterModule.forRoot(desktopRoutes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/routes/route-support/route-support.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-support',
+  templateUrl: './route-support.component.html',
+  styleUrls: ['./route-support.component.css']
+})
+export class RouteSupportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
